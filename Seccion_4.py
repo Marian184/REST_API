cdb3951e50bba910b5017ed80adbb459068abebb
Marian184 <m.fragueiro.lazcano@gmail.com>
2020-12-26 11:31:56 -0300

    cambiamos los estudiantes por items y creamos el post and get method

--- Seccion_4.py
+++ Seccion_4.py
@@ -10,7 +10,16 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        return {'item':name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+
+    def post(self,name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201 #El 201 es el codigo http que indica creado
+    #el 202 indica aceptado pero puede demorar mas en crear
 
 api.add_resource(Item,'/item/<string:name>')
 
