4fadd32c5d9a82bba4706a0880410da010b16a33
Marian184 <m.fragueiro.lazcano@gmail.com>
2020-12-26 11:46:50 -0300

    Añadimos debug, request data para cambiar el precio o establecerlo en el body de lo que se le envia, tambien añadimos la clase itemslist

--- Seccion_4.py
+++ Seccion_4.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
@@ -16,12 +16,25 @@ class Item(Resource):
         return {'item': None}, 404
 
     def post(self,name):
-        item = {'name': name, 'price': 12.00}
+        request_data = request.get_json()
+        #Si colocamos entre parentesis force=True ignora la cabecera, es peligroso
+        #pero si colocamos silent = True, me devuelve un None si la cabecera es incorrecta
+        #asi obtenemos el Json payload
+        #Si el request no tiene un dato o un json format, va a dar error
+        #En el postman tengo que tener bien el content-Type y el tipo de aplicacion
+
+        item = {'name': name, 'price': request_data['price']}
         items.append(item)
         return item, 201 #El 201 es el codigo http que indica creado
     #el 202 indica aceptado pero puede demorar mas en crear
 
+class Itemlist(Resource):
+    def get(self):
+        return {'items': items}
+
+
 api.add_resource(Item,'/item/<string:name>')
+api.add_resource(Itemlist, '/items')
 
 
-app.run(port=8000)
+app.run(port=8000, debug=True)
