ec2054416665c961a955283c6063f2f9b511c8b0
Marian184 <m.fragueiro.lazcano@gmail.com>
2020-12-26 12:48:06 -0300

    Cambiar los diccionarios por objetos en user.py, cambiar el iterar por una list comphrehention y quitar el == porque puede cambiar segun la verison de python

--- security.py
+++ security.py
@@ -1,30 +1,28 @@
-users = [
-    {
-        'id': 1,
-        'username': 'bob',
-        'password': '123456'
-    }
-]
-username_mapping = {
-    'bob':{
-            'id': 1,
-            'username': 'bob',
-            'password': 'asdf'
-    }
-}
+from user import User
+from werkzeug.security import safe_str_cmp
 
-userid_mapping = {
-    1:{
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
+
+users = [
+    User(1, 'bob', '123456')
+]
+username_mapping = {u.username: u for u in users}
+
+userid_mapping = {u.id: u for u in users}
+
+
+# userid_mapping = {
+#     1:{
+#         'id': 1,
+#         'username': 'bob',
+#         'password': 'asdf'
+#     }
+# }
 
 
 def authenticate(username, password):
     user = username_mapping.get(username, None)
-    if user and user.password == password:
+    # if user and user.password == password:
+    if user and safe_str_cmp(user.password, password):
         return user
 
 
